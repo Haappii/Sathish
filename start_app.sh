56fd9d46cd3933d41cee437f3cb197c67e4b8fdf
Sathish Kumar <sathishheternal@gmail.com>
2026-02-20 00:42:29 +0530

    Change frontend port default to 5180

--- start_app.sh
+++ start_app.sh
@@ -9,7 +9,12 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 BACKEND_HOST="${BACKEND_HOST:-0.0.0.0}"
 BACKEND_PORT="${BACKEND_PORT:-8000}"
 FRONTEND_HOST="${FRONTEND_HOST:-0.0.0.0}"
-FRONTEND_PORT="${FRONTEND_PORT:-5173}"
+FRONTEND_PORT="${FRONTEND_PORT:-5180}"
+
+# Used by the desktop-app build/run (Electron wrapper) to know which web URL to load.
+# For EC2, set PUBLIC_HOST to your public IP or domain.
+PUBLIC_HOST="${PUBLIC_HOST:-localhost}"
+export APP_URL="${APP_URL:-http://${PUBLIC_HOST}:${FRONTEND_PORT}}"
 
 # --- Backend ---
 cd "${ROOT_DIR}/backend"
